d325108c1e0b16b5db6d5ad657c2c9fadccd44a5
Angelique Ulep <ulepangelique@gmail.com>
2023-03-22 11:59:23 -0400

    Module 9 Activity 1

---
+++ module-09/assignments/people-xhr-promise.js
@@ -0,0 +1,68 @@
+const xhr = (url, method) =>
+  new Promise((resolve) => {
+    const xhrhttp = new XMLHttpRequest();
+    xhrhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        resolve(this.responseXML);
+      }
+    };
+    xhrhttp.open(method, url, true);
+    xhrhttp.send();
+  });
+
+function stringToNode(value) {
+  const html = value.trim();
+  const template = document.createElement(`template`);
+  template.innerHTML = html;
+  return template.content.firstChild;
+}
+
+function createPerson(info) {
+  const content = `
+    <tr>
+      <td>${info.id}</td>
+      <td>
+        ${info.firstName} ${info.lastName}
+      </td>
+      <td>${info.email}</td>
+      <td>${info.gender}</td>
+      <td>${info.ipAddress}</td>
+    </tr>
+  `;
+
+  return stringToNode(content);
+}
+
+function parseInfo(person) {
+  const id = person.getElementsByTagName(`id`)[0].childNodes[0].nodeValue;
+  const firstName = person.getElementsByTagName(`firstName`)[0].childNodes[0].nodeValue;
+  const lastName = person.getElementsByTagName(`lastName`)[0].childNodes[0].nodeValue;
+  const email = person.getElementsByTagName(`email`)[0].childNodes[0].nodeValue;
+  const gender = person.getElementsByTagName(`gender`)[0].childNodes[0].nodeValue;
+  const ipAddress = person.getElementsByTagName(`ipAddress`)[0].childNodes[0].nodeValue;
+  return {
+    id,
+    firstName,
+    lastName,
+    email,
+    gender,
+    ipAddress
+  };
+}
+
+function displayData(xmlDoc) {
+  const people = xmlDoc.getElementsByTagName(`person`);
+  const tableElement = document.getElementById(`pTable`);
+
+
+  for (let index = 0; index < people.length; index++) {
+    const person = people[index];
+
+    const parsedInfo = parseInfo(person);
+    const personElement = createPerson(parsedInfo);
+
+    tableElement.appendChild(personElement);
+  }
+}
+
+xhr(`../../module-05/assignments/people.xml`, `GET`).then((data) => displayData(data));
